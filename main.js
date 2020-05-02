ea68bdfbaf0f015fbd538c171e1ae48d2aae955e
tariksitki <tariksitki0119@gmail.com>
2020-05-02 22:23:42 +0200

    02.05.2020

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 
+function Welcome () {
+
+    alert ("Welcome" )
+    var isim =  prompt ( "Please enter your name" , "tarik")
+
+    document.getElementById ("user").innerHTML=isim
+
+
+
+
+    console.log(isim.length);
+
+
+
+
+
+    if ( isim != null ) {
+
+        var message = " Hello " + isim + " How are you"
+        alert (message)
+        
+    }
+    
+
+   
+
+}
