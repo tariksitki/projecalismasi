86722d589ca8b4c65aaa1997e70ff33fdadb7e31
tariksitki <tariksitki0119@gmail.com>
2020-05-03 15:44:15 +0200

    Kurumsal seite

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 
-  function Welcome () {
+  //function Welcome () {
 
-   alert ("Hallo Wilkommen")
+  // alert ("Hallo Wilkommen")
 
-    var isim = prompt("Hallo wilkommen bei unserem Homepage" , "Bitte schreiben Sie name")
+ //   var isim = prompt("Hallo wilkommen bei unserem Homepage" , "Bitte schreiben Sie name")
 
-    var message = "hallo" + isim + "how are you"
+ //   var message = "hallo" + isim + "how are you"
  
-    alert(message)
+ //   alert(message)
 
-    if (isim != null) {
+  //  if (isim != null) {
 
-      console.log(isim.length);
+  //    console.log(isim.length);
 
-        document.getElementById("user").innerHTML = isim
+ //       document.getElementById("user").innerHTML = isim
 
-    }
+ //   }
 
-   if (isim.length > 0) {
+ //  if (isim.length > 0) {
 
-        console.log("sifirdan buyuk bir isim girdiniz" );
+  //      console.log("sifirdan buyuk bir isim girdiniz" );
 
-        alert("sifirdan buyuk bir isim girdiniz")
+ //       alert("sifirdan buyuk bir isim girdiniz")
         
-    }
-     }
+ //   }
+ //    }
